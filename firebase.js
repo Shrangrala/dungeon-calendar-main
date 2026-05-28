08837816ba9d8aee23372fbf11f9e41f25c3f6cb
Shrangrala <155485748+Shrangrala@users.noreply.github.com>
2026-05-28 14:03:21 -0500

    Fix Firebase config

--- firebase.js
+++ firebase.js
@@ -1,20 +1,17 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCRSwIQxC_gpic-Z4o0Rb6mPhkf1yBguGI",
   authDomain: "dungeon-calendar-app.firebaseapp.com",
   projectId: "dungeon-calendar-app",
   storageBucket: "dungeon-calendar-app.firebasestorage.app",
-  messagingSenderId: "575544890085",
-  appId: "1:1089961645011:web:07da2f00587b54d41e5526"
+  messagingSenderId: "1089961645011",
+  appId: "1:1089961645011:web:07da2f00587b54d41e5526",
+  measurementId: "G-YRP7187GYT"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export default app;
